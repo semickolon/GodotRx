abb6a35937ee8d2ff4ea1949f32c54e088dd930e
semickolon <mickolenulud@gmail.com>
2020-10-14 16:10:45 +0800

    Extract part of NodeExtensions to ObjectExtensions

--- addons/godotrx/NodeExtensions.cs
+++ addons/godotrx/NodeExtensions.cs
@@ -1,70 +1,13 @@
 using Godot;
 using GodotRx.Internal;
 using System;
-using System.Collections.Generic;
 using System.Reactive;
 using System.Reactive.Linq;
-
-using Object = Godot.Object;
 
 namespace GodotRx
 {
   public static class NodeExtensions
   {
-    public static IObservable<Unit> ObserveSignal(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker());
-    
-    public static IObservable<T> ObserveSignal<T>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T>());
-
-    public static IObservable<(T1, T2)> ObserveSignal<T1, T2>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T1, T2>());
-
-    public static IObservable<(T1, T2, T3)> ObserveSignal<T1, T2, T3>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T1, T2, T3>());
-
-    public static IObservable<(T1, T2, T3, T4)> ObserveSignal<T1, T2, T3, T4>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T1, T2, T3, T4>());
-
-    public static IObservable<(T1, T2, T3, T4, T5)> ObserveSignal<T1, T2, T3, T4, T5>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T1, T2, T3, T4, T5>());
-
-    public static IObservable<(T1, T2, T3, T4, T5, T6)> ObserveSignal<T1, T2, T3, T4, T5, T6>(this Object obj, string signalName)
-      => _ObserveSignal(obj, signalName, new EventTracker<T1, T2, T3, T4, T5, T6>());
-
-    private static IObservable<T> _ObserveSignal<T>(Object obj, string signalName, BaseEventTracker<T> tracker)
-    {
-      obj.Connect(signalName, tracker, tracker.TargetMethod);
-
-      var subscriptionList = new List<IDisposable>();
-      var onSignal = tracker.OnSignal;
-      var instId = obj.GetInstanceId();
-      
-      InstanceTracker.Of(obj).Freed += () =>
-      {
-        // GD.Print($"freed {instId}:{signalName}");
-        subscriptionList.ForEach(s => s.Dispose());
-        tracker.DeferredFree();
-      };
-
-      return Observable.Create<T>(observer => 
-      {
-        var subscription = onSignal.Subscribe(observer.OnNext);
-        subscriptionList.Add(subscription);
-
-        return () =>
-        {
-          subscription.Dispose();
-          subscriptionList.Remove(subscription);
-        };
-      });
-    }
-
-    public static void DeferredFree(this Object obj)
-    {
-      obj.CallDeferred("free");
-    }
-
     public static IObservable<float> OnProcess(this Node node)
       => node.GetNodeTracker().OnProcess;
 
